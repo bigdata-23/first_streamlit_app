c293be7310147655b6abdabf9a28f10da8868dcd
bigdata-23 <125269109+bigdata-23@users.noreply.github.com>
2023-06-07 21:42:30 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -35,10 +35,10 @@ streamlit.dataframe(fruits_to_show)
 
 streamlit.header("Fruityvice Fruit Advice!")
 import requests
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/kiwi")
 
 # tahe json and normalize 
-fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+fruityvice_normalized = pd.json_normalize(fruityvice_response.json())
 
 # output it to screen
 streamlit.dataframe(fruityvice_normalized)
